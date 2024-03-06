99996012d1c2ce43415d148ef2f3b449cf32274d
Jonathan Rosas <jonathanroscedprogramador@gmail.com>
2024-03-05 20:03:48 -0600

    Programa Para Calcular el area de un Triangulo

---
+++ main.py
@@ -0,0 +1,8 @@
+#CALCULAR EL AREA DE UN TRIANGULO
+##########################################
+
+
+Altura=int(input("Ingresa Tu Altura: "))
+Base=int(input("Ingresa Tu Base: "))
+
+print("Area: ",Altura*Base/2)
