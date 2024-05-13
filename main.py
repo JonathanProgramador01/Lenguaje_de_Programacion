2b0d51b091dbd64d1287e59d2ffac9c2b92308aa
Jonathan Rosas Cedillo <jonathanroscedprogramador@gmail.com>
2024-05-13 08:57:12 -0600

    Agregando metodo de gauss jordan y guass seidel

---
+++ main.py
@@ -0,0 +1,36 @@
+dic = {"x":[12,3,41],
+       "y":[4,23,1]}
+
+for i in dic:
+    print(dic[i][0])
+
+
+
+
+
+
+
+
+
+
+
+# records = (
+#     (3, '101', 'Spam'),
+#     (7, '422', 'Eggs'),
+#     (4, '631', 'Spam, spam, eggs, and spam')
+# )
+
+# table = document.add_table(rows=1, cols=3)
+# hdr_cells = table.rows[0].cells
+# hdr_cells[0].text = 'Qty'
+# hdr_cells[1].text = 'Id'
+# hdr_cells[2].text = 'Desc'
+# for qty, id, desc in records:
+#     row_cells = table.add_row().cells
+#     row_cells[0].text = str(qty)
+#     row_cells[1].text = id
+#     row_cells[2].text = desc
+
+# document.add_page_break()
+
+# document.save('demo.docx')
