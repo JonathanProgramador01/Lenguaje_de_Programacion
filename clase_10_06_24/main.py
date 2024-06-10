1cb6299e1a885e457afc16f4181b97c0c753e422
Jonathan Rosas Cedillo <jonathanroscedprogramador@gmail.com>
2024-06-10 08:06:21 -0600

    Agregando el trabajo de la clase de hoy

---
+++ clase_10_06_24/main.py
@@ -0,0 +1,42 @@
+###########PRIMERR PROGRAMAAAAA#############
+print("\nPRIMER PROGRAMAA\n")
+suma = 0
+for i in range(1,101):
+    suma += i
+print(suma)
+
+
+###############SEGUNDO PROGRAMAAAA ########################
+print("\nSEGUNDO PROGRAMA\n")
+
+for i in range(11):
+    for j in range(1,11):
+        print(i,"*",j,"= ",i*j)
+    print("\n")
+    
+###################TERCER PROGRAMAAA ##############################
+print("\nTERCER PROGRAAMAAAA\n")
+##triangulo Datoss##
+BASE = 12.3
+ALTURA = 4.5
+
+
+print("TRIANGULO")
+print("Base: ",BASE," Altura: ",ALTURA)
+print("AREA: ",BASE*ALTURA)
+
+##Cuadradooo datosss###
+LADO = 3
+
+print("\nCUADRADOO")
+print("LADO: ",LADO)
+print("AREA: ",LADO*LADO)
+
+
+
+
+
+
+
+
+    
---
+++ clase_10_06_24/pandas_.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def generar_corazon(t,escala=1.0):
+    x = escala*(16*np.sin(t)**3)
+    y =escala*(13*np.cos(t)-5*np.cos(2*t)-2*np.cos(3*t)-np.cos(4*t))
+    return x,y
+
+data = {
+    "centro_x": [0,10,-10,5,-5],
+    "centro_y": [0,10,-10,-5,5],
+    "escala": [1,0.5,0.75,1.25,1]
+    
+}
+
+df = pd.DataFrame(data)
+
+
+fig, ax = plt.subplots()
+t = np.linspace(0, 2 * np.pi, 1000)  
+
+
+for _, row in df.iterrows():
+    x, y = generar_corazon(t, row["escala"])
+    ax.plot(x + row["centro_x"], y + row["centro_y"])
+
+
+ax.set_aspect("equal")
+ax.set_xlim(-20, 20)
+ax.set_ylim(-20, 20)
+
+
+plt.show()
