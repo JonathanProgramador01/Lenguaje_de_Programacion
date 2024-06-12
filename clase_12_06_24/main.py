b3f8d565a5575f6bf36c096194794a18d1044c32
Jonathan Rosas Cedillo <157512868+JonathanProgramador01@users.noreply.github.com>
2024-06-12 08:33:30 -0600

    Update main.py

--- clase_12_06_24/main.py
+++ clase_12_06_24/main.py
@@ -6,13 +6,8 @@ def operacion (dato):
     return f"Tienes {a} microdiscos"
 
     
-    
-
-
-
 print("##############################Calculadora de Microdiscoss########################################")
 microdiscos = float(input("\n\nIingrese el numero de Gybytes de tu disco duro:  "))
-#SUPONIENDO QUE MI DISCO DURO ES DE 100 GYBYTES
 print(operacion(microdiscos))
 
 
